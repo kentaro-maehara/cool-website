a78a0d44e076f9d53310d1702edd4839727f33c0
Kentaro Maehara <kentaro_maehara@so-netmedia.jp>
2021-09-02 19:59:00 +0900

    favicon.icoの追加

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
-import CourseGoalList from "./components/CourseGoals/CourseGoalList/CourseGoalList";
-import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
 import "./App.css";
 import "./swatch/bootstrap.css";
-import mchammer from "./img/test/mchammer.gif";
-import selfIcon from "./img/self_image.png";
 import Navbar from "./components/UI/Navbar/Navbar";
 import Main from "./components/Main/Main";
-
-const Kentaro = () => {
-  return <img src={selfIcon} alt="" className="icon"></img>;
-};
-
-const Mchmmer = () => {
-  return <img src={mchammer} alt="counter"></img>;
-};
 
 const App = () => {
   return (
--- src/components/Main/SelfIntroduction.js
+++ src/components/Main/SelfIntroduction.js
@@ -1,17 +1,34 @@
-import React from "react";
-import icon from "../../img/self_image.png";
+import React, { useState } from "react";
+import icon from "../../img/self_icon.jpg";
+import selfIcon from "../../img/selfIcon.png";
 import "./SelfIntroduction.css";
 
 const Icon = () => {
   return <img src={icon} className="icon"></img>;
 };
 
+const SelfIcon = () => {
+  return <img src={selfIcon} className="icon"></img>;
+};
+
 const SelfIntroduction = () => {
+  const [trueIcon, setTrueIcon] = useState(false);
+
+  const iconFalse = () => {
+    setTrueIcon(false);
+  };
+  const changeIcon = () => {
+    setTrueIcon(true);
+    setTimeout(iconFalse, 2000);
+  };
   return (
     <div>
       <div className="page-header"></div>
       <h2>Who is Kentaro Maehara anyway?</h2>
-      <Icon />
+      <button className="btn btn-info" onClick={changeIcon}>
+        true figure...
+      </button>
+      {trueIcon ? <SelfIcon /> : <Icon />}
     </div>
   );
 };
